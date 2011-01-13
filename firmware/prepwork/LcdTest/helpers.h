cd65ba4cf17c210031a2ac7622d42e0f964d578d
chnedward <chnedward@d36fda19-c931-eebf-9906-6b906979d43a>
2011-01-13 09:25:03 +0000

    图形驱动：目前已经初步实现画点和画线

---
+++ firmware/prepwork/LcdTest/helpers.h
@@ -0,0 +1,19 @@
+#ifndef HELPER_TYPES_H_
+#define HELPER_TYPES_H_
+
+
+#include "helper_bits.h"
+
+typedef unsigned char uint8;
+
+/*
+void delay(int x)
+{
+    int i;
+    for (i = 0; i < x; ++x) {
+        ;
+    }
+}
+*/
+
+#endif /*HELPER_TYPES_H_*/
