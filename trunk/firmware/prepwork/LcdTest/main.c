6c8b1f45b0746d88e5542b0db7af0a1af3d3d867
chnedward <chnedward@d36fda19-c931-eebf-9906-6b906979d43a>
2011-01-18 14:15:34 +0000

    主界面

--- trunk/firmware/prepwork/LcdTest/main.c
+++ trunk/firmware/prepwork/LcdTest/main.c
@@ -9,31 +9,23 @@ void main(void)
 	WDTCTL = WDTPW + WDTHOLD;
 	
 	initLcd();
+
+    // main menu background and icons
+    drawPicture(res_appleLogo, sizeof(res_appleLogo));
+    delay(2000);
+    initGraph();
+    drawPicture(res_mainmenu, sizeof(res_mainmenu));
     
-//#define LINE_TEST_1
-//#define LINE_TEST_2
-#define PIC_TEST_1
-
-#ifdef LINE_TEST_1
-    int i;
+    // main menu chars
+    setCharPos(0, 3);
+    writeStr("MENU");
+    setCharPos(3, 0);
+    writeStr(" Mail");
+    setCharPos(3, 3);
+    writeStr(" NFC");
+    setCharPos(3, 6);
+    writeStr("Game");
     
-    for (i = 0; i < LCD_PIXEL_Y; i += 5) {
-        drawLine(0, i, LCD_PIXEL_X - 1, LCD_PIXEL_Y - i);
-    }
-#endif
-
-#ifdef LINE_TEST_2
-    int i;
-    
-    for (i = 0; i < LCD_PIXEL_Y; ++i) {
-        drawLine(0, i, LCD_PIXEL_X - 1, i);
-    }
-#endif
-
-#ifdef PIC_TEST_1
-    drawPicture(res_pic1, sizeof(res_pic1));
-#endif
-
     do {
         ;
     } while (1);
