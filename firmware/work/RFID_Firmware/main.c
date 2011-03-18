25009c8adffeab2aa49dc4a064645fada89b21c5
chn.edward <chn.edward@gmail.com>
2011-03-18 13:15:28 +0000

    key board driver

--- firmware/work/RFID_Firmware/main.c
+++ firmware/work/RFID_Firmware/main.c
@@ -2,17 +2,35 @@
 #include <string.h>
 #include "helpers.h"
 #include "driver_lcd.h"
+#include "driver_kbd.h"
 #include "res_picture.h"
 
 void main(void)
 {
 	WDTCTL = WDTPW + WDTHOLD;
 	
+	// LCD driver
 	initLcd();
-    
-	drawPicture(res_appleLogo, sizeof(res_appleLogo));
+	
+	writeStr("Hello");
+	
+	do {
+        ;
+    } while (1);
+	
+	// Set timer for keyboard scan
+	CCTL0 = CCIE; // CCR0 interrupt enabled
+	CCR0 = 10000; // 10ms
+	TACTL = TASSEL_2 + MC_1; // SMCLK, contmode
+	_BIS_SR(LPM0_bits + GIE);                 // Enter LPM0 w/ interrupt
 	
     do {
         ;
     } while (1);
+}
+
+#pragma vector=TIMERA0_VECTOR
+__interrupt void Timer_A (void)
+{
+	kbd_scanner();
 }
